0dc966890626ce501d18856070fc2f1c2440c56f
Oliver Caha <74534592+9551-Dev@users.noreply.github.com>
2022-08-24 17:58:36 +0200

    Update isometrih.lua

--- isometrih.lua
+++ isometrih.lua
@@ -336,7 +336,7 @@ local function init(terminal)
     end
 
     function methods.fill_blocks(tile,start_x,start_y,start_z,end_x,end_y,end_z)
-        expect(1,tile,"string","table")
+        expect(1,tile,"string","table","nil")
         if start_x > end_x then start_x,end_x = end_x,start_x end
         if start_y > end_y then start_y,end_y = end_y,start_y end
         if start_z > end_z then start_z,end_z = end_z,start_z end
